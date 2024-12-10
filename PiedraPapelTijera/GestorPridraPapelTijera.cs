0a8fce394ae9427a3baecbed0275fc1469d48862
smorcor744 <mcsergio@iesrafaelalberti.es>
2024-12-10 21:29:17 +0100

    Initial commit

---
+++ PiedraPapelTijera/GestorPridraPapelTijera.cs
@@ -0,0 +1,6 @@
+namespace PiedraPapelTijera;
+
+public class GestorPridraPapelTijera
+{
+    
+}
---
+++ PiedraPapelTijera/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+public class GestorPridraPapelTijera
+{
+    static List<Movimientos> movimientos = new List<Movimientos>();
+
+    static void Main()
+    {
+        GetMovement(2);
+
+    }
+
+    static void GetMovement(int jugadores)
+    {
+        for (int i = 0; i < jugadores; i++)
+        {
+            Thread t = new Thread(() =>
+                {
+                    movimientos.Add(Movimiento());
+                }
+                );
+            t.Start();
+        }
+    }
+
+    static Movimientos Movimiento()
+    {
+        Random random = new Random();
+
+        int movimientoAleatorio = random.Next(0, 2);
+        return (Movimientos)movimientoAleatorio;
+    }
+
+    enum Movimientos
+    {
+        Papel,
+        Piedra,
+        Tijera
+    }
+}
